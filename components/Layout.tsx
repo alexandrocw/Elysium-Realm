e4173db1335a243114b072746015b1d53490e7ef
Alexandro C.W <alexandrowijaya.aw2@gmail.com>
2023-02-16 21:48:47 +0700

    Add ads to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+import Script from "next/script";
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
@@ -8,13 +10,16 @@ interface Props {
 
 const Layout = ({ children, ...props }: Props) => {
   return (
-    <div>
+    <>
+      <Head>
+        <Script data-cfasync="false" src="//db033pq6bj64g.cloudfront.net/?bqpbd=977886" />
+      </Head>
       <div className="min-h-screen flex flex-col body-bg">
         <Navbar />
         { children }
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
