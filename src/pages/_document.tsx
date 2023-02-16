9c8d8e584a55dab1479d99def3a39f96b40adcbb
Alexandro C.W <alexandrowijaya.aw2@gmail.com>
2023-02-16 21:44:34 +0700

    Add ads

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Head, Main } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <script
+            data-cfasync="false"
+            src="//db033pq6bj64g.cloudfront.net/?bqpbd=977886"
+          />
+        </Head>
+        <body>
+          <Main />
+        </body>
+      </html>
+    );
+  }
+}
+
+export default MyDocument;
