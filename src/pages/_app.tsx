1fd4ff38b161cec5bb98a17de3b2e02a0eb72803
Alexandro C.W <alexandrowijaya.aw2@gmail.com>
2023-03-23 01:55:50 +0700

    Change adsense code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,20 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Link from 'next/link'
 import { SessionProvider } from "next-auth/react";
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Head>
-        <link rel='icon' href="/favicon.png" />
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>    
-    </SessionProvider>
+    <>
+      <Script id='Adsense-id' data-ad-client='ca-pub-4777140716080479' async strategy='afterInteractive' onError={(e) => {console.error('Script failed to load', e)}} src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' />
+      <SessionProvider session={pageProps.session}>
+        <Head>
+          <link rel='icon' href="/favicon.png" />
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>    
+      </SessionProvider>
+    </>
   )
 }
