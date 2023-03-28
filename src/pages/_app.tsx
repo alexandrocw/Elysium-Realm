20eef9f122261456cc76b1d131b98e4775bb0bc4
Alexandro C.W <alexandrowijaya.aw2@gmail.com>
2023-03-28 21:53:31 +0700

    Change adsense code to txt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,13 @@ import '@/styles/globals.css'
 import Layout from 'components/Layout'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import Link from 'next/link'
 import { SessionProvider } from "next-auth/react";
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   const imageUrl = "public/thumbnail.png";
 
   return (
     <>
-      <Script id='Adsense-id' data-ad-client='ca-pub-4777140716080479' async strategy='afterInteractive' onError={(e) => {console.error('Script failed to load', e)}} src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' crossOrigin='anonymous' />
       <SessionProvider session={pageProps.session}>
         <Head>
           <link rel='icon' href="/favicon.png" />
